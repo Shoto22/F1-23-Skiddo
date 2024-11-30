78b0bc9f972df1902823b2349378e539aa44ca08
Shoto22 <135430333+Shoto22@users.noreply.github.com>
2024-11-30 15:27:24 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -98,5 +98,5 @@ document.addEventListener('DOMContentLoaded', () => {
     agregarVideoImagen('https://www.youtube.com/embed/q9Bv0B-wJ5s', 'video');
 
     // Añadir imágenes desde GitHub con un pie de foto
-    agregarVideoImagen('imagenes/quevedo-gana-bahrain.jpg', 'img', 'Quevedo gana el GP de Bahrain');
+    agregarVideoImagen('Imgs/Bahrain.jpg', 'img', 'Quevedo gana el GP de Bahrain');
 });
